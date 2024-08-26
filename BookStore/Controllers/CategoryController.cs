f077de61e64bccb8b1f96bc93b839e090452ef8c
@moos93 <mosbsh97@gmail.com>
2024-08-26 15:17:37 +0300

    Add project files.

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> CategoryList= _db.Categories.ToList();
+            return View(CategoryList);
+        }
+    }
+}
